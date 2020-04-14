e940bb243629ce8ca78a30fdd5a39bb8403b40a5
toshinori sato <tyahhahha@gmail.com>
2020-04-14 18:31:00 +0900

    add header component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from "next/link";
+import Header from "../components/Header";
 
 export default function Index() {
   return (
     <div>
-      <Link href="/about">
-        <a>About Page</a>
-      </Link>
+      <Header />
       <p>Hello Next.js</p>
     </div>
   );
