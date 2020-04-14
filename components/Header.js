a9124288e769e378f498a344e149ebf2b74df745
toshinori sato <tyahhahha@gmail.com>
2020-04-14 18:39:34 +0900

    add layout component

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,8 @@ const Header = () => (
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
+    <Link href="/about">
+      <a style={linkStyle}>About</a>
     </Link>
   </div>
 );
